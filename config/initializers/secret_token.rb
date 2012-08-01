9ad144548e1fe3c8bd5be3162583041b53bc6f14
Dmitry Nix <dmitry.nix@gmail.com>
2012-08-01 16:25:24 -0300

    `rails new organize-app`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrganizeApp::Application.config.secret_token = 'addcbbbf6d0f51e541c13a503ad3e9ef257e9e668d73597021d087cc7cd0d0c505f3960f4ba3fa963f2fd4444da807485bad20f7b90594288e249496b74f28b2'
