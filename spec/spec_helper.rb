478a73867f55489aacff6f79014988748b836444
Dmitry Nix <dmitry.nix@gmail.com>
2012-08-18 13:16:15 -0300

    "optimize" rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,9 @@ end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+#Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+Rails.logger.level = 4
 
 RSpec.configure do |config|
   # ## Mock Framework
