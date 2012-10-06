633a88f923fbeb4ffd607931c0aee30ff5c3948e
Dmitry Nix <dmitry.nix@gmail.com>
2012-10-06 14:54:58 -0300

    Fix application_helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -9,5 +9,16 @@ describe ApplicationHelper do
     period_url(period).should eq '/purchases?year=2012&month=02'
   end
 
-  it 'should return tags with link'
+  context 'tags link' do
+    let :tags do
+      [
+        double('Tag#1', :name => 'tag1'),
+        double('Tag#2', :name => 'tag2')
+      ]
+    end
+
+    it 'should return tags with link' do
+      expect(tags_with_link(tags)).to eq "<a href=\"#\">tag1</a> and <a href=\"#\">tag2</a>"
+    end
+  end
 end
