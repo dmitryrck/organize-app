7d0eb0605ccaf153471adc618fa4cb9e299e95b2
Dmitry Nix <dmitry.nix@gmail.com>
2012-10-07 21:38:10 -0300

    Fix migration to add purchases counter cache column

--- db/migrate/20121008001854_add_purchases_count_to_tags.rb
+++ db/migrate/20121008001854_add_purchases_count_to_tags.rb
@@ -1,9 +1,5 @@
 class AddPurchasesCountToTags < ActiveRecord::Migration
   def change
     add_column :tags, :purchases_count, :integer, :default => 0
-
-    Tag.all.each do |tag|
-      tag.update_column :purchases_count, tag.purchases.count
-    end
   end
 end
