d6289c93c3550a13bcce99dc393b12e246e783e0
Dmitry Nix <dmitry.nix@gmail.com>
2012-11-08 13:29:03 -0300

    Add tiny info about currency fields

--- app/inputs/currency_input.rb
+++ app/inputs/currency_input.rb
@@ -1,8 +1,10 @@
 class CurrencyInput < SimpleForm::Inputs::Base
+  # TODO: Refactory hint
   def input
     [
       '<span class="add-on">R$ </span>',
-      @builder.text_field(attribute_name, input_html_options)
+      @builder.text_field(attribute_name, input_html_options),
+      "<br>#{I18n.t('simple_form.hints.purchase.value')}"
     ].join.html_safe
   end
 end
